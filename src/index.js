6d4c68a85d77cebbde31a4fa52e6e9844362a35c
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2021-05-14 00:36:13 -0300

    pegar preço do time do flamengo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
-// "start": "nodemon src/index"
+
+
+By = webdriver.By;
+// until = webdriver.until
 
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
 (async function exemplo() {
+
     let driver = new webdriver.Builder().forBrowser('chrome').build();
-    driver.get('https://www.google.com/');
+    driver.get('https://www.transfermarkt.com.br/flamengo-rio-de-janeiro/startseite/verein/614');
+
+    driver.findElement(By.css('.dataMarktwert')).then((e) => {
+
+        console.log('sucess');
+
+        e.getText().then(function (text) {
+
+            console.log("valor de mercado do flamengo: " + text);
+
+         });
+    });
+
 })();
 
 
